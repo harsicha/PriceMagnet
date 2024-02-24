ab21a28b90e3d49da46366119ff19c3cc4d65f61
harsicha <16000485+harsicha@users.noreply.github.com>
2024-02-24 22:25:59 +0530

    Add UI to VC

---
+++ PriceComparisonUI/src/App.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+import './App.css';
+import { Main } from './main.js';
+import { Header, Filters } from './main.js';
+import { Sources } from './constants.js';
+
+function App() {
+  return (
+    <div>
+      <Main></Main>
+    </div>
+  );
+}
+
+export default App;
